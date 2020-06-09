7d2591f0a8c24032b38110b85a13cbd7266ce5cb
Aaron Weber <aqwertyweber@gmail.com>
2020-06-09 13:03:59 -0500

    removed list files

--- DojoManagmentSystem/DojoManagmentSystem/Infastructure/Extensions/HtmlExtensions.cs
+++ DojoManagmentSystem/DojoManagmentSystem/Infastructure/Extensions/HtmlExtensions.cs
@@ -1,10 +1,6 @@
-﻿using DojoManagmentSystem.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web;
+﻿using System;
 using System.Web.Mvc;
+//using System.Web.Mvc.Html.LabelExtensions;
 
 namespace DojoManagmentSystem.Infastructure.Extensions
 {
@@ -20,5 +16,17 @@ namespace DojoManagmentSystem.Infastructure.Extensions
 
             return false;
         }
+
+        //public static IHtmlString IsCurrentAction(this HtmlHelper helper, string actionName, string controllerName)
+        //{
+        //    string currentControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+        //    string currentActionName = (string)helper.ViewContext.RouteData.Values["action"];
+
+        //    if (currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase) && currentActionName.Equals(actionName, StringComparison.CurrentCultureIgnoreCase))
+        //        return true;
+        //    helper.LabelForModel()
+        //    return false;
+        //}
+
     }
 }
